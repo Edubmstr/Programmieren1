cad791b90dfe12831e85dc6c314702ba595f49a9
Edubmstr <153729455+Edubmstr@users.noreply.github.com>
2024-01-06 19:25:49 +0100

    Add files via upload

---
+++ Übungen/UngeradeZahlen.java
@@ -0,0 +1,12 @@
+package Übungen;
+
+public class UngeradeZahlen {
+    public static void main(String[] args){
+        for (int i = 0; i <= 100; i++) {
+            if(!(i % 2 == 0 )){
+                System.out.println(i);
+            }
+        }
+
+    }
+}
