3cf50fa17d5869816dc93d07ff36dd744b10f68b
Eduard Baumeister <eduard.baumeister1@gmail.com>
2024-01-07 17:26:45 +0100

    Exception

--- OnlineShop/Storage.java
+++ OnlineShop/Storage.java
@@ -24,7 +24,7 @@ public class Storage implements Sorting {
                 }
             }
         }
-        throw new NoProductException();
+        throw new NoProductException(product);
     }
 
     public ArrayList<Product> getProducts(){
